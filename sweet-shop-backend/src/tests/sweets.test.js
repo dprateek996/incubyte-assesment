36e7ff316df44492db4f4475bc0f6e495351b59f
Prateek Dwivedi <dprateek996@gmail.com>
2025-12-13 12:03:07 +0530

    test: add failing test for add sweet endpoint (RED)

---
+++ sweet-shop-backend/src/tests/sweets.test.js
@@ -0,0 +1,38 @@
+const request = require("supertest");
+const app = require("../app");
+
+let token;
+
+beforeAll(async () => {
+
+    await request(app)
+    .post("/api/auth/register")
+    .send({
+      email: "sweetuser@example.com",
+      password: "password123"
+    });
+
+    const res = await request(app)
+    .post("/api/auth/login")
+    .send({
+      email: "sweetuser@example.com",
+      password: "password123"
+    });
+
+  token = res.body.token;
+});
+
+it("should add a new sweet", async () => {
+  const res = await request(app)
+    .post("/api/sweets")
+    .set("Authorization", `Bearer ${token}`)
+    .send({
+      name: "Gulab Jamun",
+      price: 10,
+      quantity: 20
+    });
+
+  expect(res.statusCode).toBe(201);
+  expect(res.body).toHaveProperty("id");
+  expect(res.body.name).toBe("Gulab Jamun");
+});
